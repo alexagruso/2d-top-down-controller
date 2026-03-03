e513a8e83c25ad39771b7407c88c74bdc2d1a067
alex <agrusoalexander@gmail.com>
2026-03-02 19:10:25 -0600

    physics frame step plugin

--- src/debug.rs
+++ src/debug.rs
@@ -1,4 +1,5 @@
-use bevy::prelude::*;
+use avian2d::prelude::{Physics, PhysicsTime};
+use bevy::{input::common_conditions::input_just_pressed, prelude::*};
 
 mod camera_zoom;
 mod fps_overlay;
@@ -16,6 +17,33 @@ impl Plugin for DebugPlugin {
             CameraZoomPlugin,
             FpsOverlayPlugin::new(Color::srgb(1.0, 1.0, 0.0)),
             WindowEscapePlugin,
+            FrameStepPlugin,
         ));
     }
+}
+
+struct FrameStepPlugin;
+
+impl Plugin for FrameStepPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            Update,
+            (
+                toggle_paused.run_if(input_just_pressed(KeyCode::KeyP)),
+                step.run_if(input_just_pressed(KeyCode::Enter)),
+            ),
+        );
+    }
+}
+
+fn toggle_paused(mut time: ResMut<Time<Physics>>) {
+    if time.is_paused() {
+        time.unpause();
+    } else {
+        time.pause();
+    }
+}
+
+fn step(mut physics_time: ResMut<Time<Physics>>, fixed_time: Res<Time<Fixed>>) {
+    physics_time.advance_by(fixed_time.delta());
 }
